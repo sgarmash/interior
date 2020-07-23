58507e721ee8aa6f71f9d5fd3b14dd151251471b
sergey09 <sergey.garmash09@gmail.com>
2020-07-23 07:57:43 +0300

    feat ( header responsive )

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,12 @@ $(function(){
 
   $( '.burger' ).on( 'click', function(){
     $(this).toggleClass( 'active' );
+    $('.navigation').toggleClass( 'active' );
+  });
+
+  $( '.dropdown' ).on( 'click', function(event){
+    event.preventDefault();
+    $( this ).find( '.navigation-list--sub' ).toggleClass( 'active' );
   });
 
 }());
