ca435ede23a9935a3e085630006ae37e7d955175
sergey09 <sergey.garmash09@gmail.com>
2020-07-29 22:31:02 +0300

    chore ( fix dropdown & style )

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function(){ 
-  let body = $( 'body' );
+
   $( '.burger' ).on( 'click', function(){
     $( this ).toggleClass( 'active' );
     $( '.navigation' ).slideToggle( 400 ).toggleClass( 'active' );
@@ -9,6 +9,12 @@ $(function(){
   $( '.navigation-list--arrow' ).on( 'click', function(event){
     $( this ).toggleClass( 'active' );
     $( this ).siblings().toggleClass( 'active' );
+  });
+
+  $(document).on('click', function (e) {
+    if ($(e.target).closest('.navigation-list--arrow').length === 0) {
+      $( '.navigation-list--sub' ).removeClass( 'active' );
+    }
   });
 
 }());
